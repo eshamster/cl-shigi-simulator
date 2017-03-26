377862d697e8313fdad943196a1ea19ae06a74cc
eshamster <hamgoostar@gmail.com>
2017-03-26 11:55:26 +0000

    Use JavaScript libraries loader in cl-web-2d-game
    This makes the first loading heavy because pre-downloaded JavaScript
    libraries are deleted and instead download them when they are not
    exist.

--- templates/layouts/three.lisp
+++ templates/layouts/three.lisp
@@ -6,35 +6,41 @@
                 :with-markup-to-string)
   (:import-from :cl-shigi-simulator.static.js.utils
                 :load-js)
+  (:import-from :cl-web-2d-game
+                :make-src-list-for-script-tag
+                :ensure-js-files)
   (:export :with-three-layout))
 (in-package :cl-shigi-simulator.templates.layouts.three)
+
+(defparameter *application-root* (asdf:system-source-directory :cl-shigi-simulator))
+(defparameter *js-download-dir* (merge-pathnames #P"static/js/copied/" *application-root*))
 
 ;; base-path requires a slash at last
 ;; Ex. NG: ".."
 ;;     OK: "../"
 (defmacro with-three-layout ((&key title js-name (base-path "") (description nil)) &body body)
-  `(with-markup-to-string
-     (html5 (:head
-             (:title ,title)
-             (:meta :charset "UTF-8")
-             (:script :src ,(merge-pathnames "js/copied/wtf-trace.js" base-path) nil)
-             (:script :src ,(merge-pathnames "js/copied/dat.gui.min.js" base-path) nil)
-             (:script :src "https://cdnjs.cloudflare.com/ajax/libs/three.js/r73/three.js" nil)
-             (:script :src ,(merge-pathnames "js/copied/threex.keyboardstate.js" base-path) nil)
-             (:script :src ,(merge-pathnames "js/copied/stats.js" base-path) nil)
-             (:script :src (load-js ,js-name :base-path ,base-path) nil)
-             (:link :rel "stylesheet" :type "text/css"
-                    :href ,(merge-pathnames "css/shigi.css" base-path)))
-            (:body
-             (:div (:a :href "/" "Top"))
-             (:div :id "description"
-                   ,@description)
-             (:br)
-             (:div :id "stats-output")
-             (:div :id "renderer" nil)
-             (:div :id "debug" "(for Debug)")
-             (:div (:pre :id "log" "(for Log)"))
-             (:br)
-             (:div "Entityのリスト"
-                   (:dl :id "entity-tree"))
-             ,@body))))
+  `(progn
+     (ensure-js-files *js-download-dir*)
+     (with-markup-to-string
+       (html5 (:head
+               (:title ,title)
+               (:meta :charset "UTF-8")
+               (dolist (js-src (make-src-list-for-script-tag
+                                ,(merge-pathnames "js/copied/" base-path)))
+                 (markup (:script :src js-src nil)))
+               (:script :src (load-js ,js-name :base-path ,base-path) nil)
+               (:link :rel "stylesheet" :type "text/css"
+                      :href ,(merge-pathnames "css/shigi.css" base-path)))
+              (:body
+               (:div (:a :href "/" "Top"))
+               (:div :id "description"
+                     ,@description)
+               (:br)
+               (:div :id "stats-output")
+               (:div :id "renderer" nil)
+               (:div :id "debug" "(for Debug)")
+               (:div (:pre :id "log" "(for Log)"))
+               (:br)
+               (:div "Entityのリスト"
+                     (:dl :id "entity-tree"))
+               ,@body)))))
