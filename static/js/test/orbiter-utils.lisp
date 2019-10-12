f00073462f035b2d055c119b80ba50e5416ddf3a
eshamster <hamgoostar@gmail.com>
2019-10-12 15:48:53 +0000

    Add: Dummy target to test lazer orbiter

---
+++ static/js/test/orbiter-utils.lisp
@@ -0,0 +1,85 @@
+(defpackage cl-shigi-simulator/static/js/test/orbiter-utils
+  (:use :cl
+        :parenscript
+        :ps-experiment
+        :cl-web-2d-game
+        :cl-ps-ecs)
+  (:export :init-dummy-player
+           :init-dummy-target
+           :get-target-point
+           :set-target-point
+           :get-target-angle
+           :set-target-angle)
+  (:import-from :cl-shigi-simulator/static/js/tools
+                :get-param
+                :shigi-screen-width
+                :shigi-screen-height))
+(in-package :cl-shigi-simulator/static/js/test/orbiter-utils)
+
+;; --- player --- ;;
+
+(defun.ps+ init-dummy-player ()
+  (let ((player (make-ecs-entity))
+        (r (get-param :player :body-r)))
+    (add-ecs-component-list
+     player
+     (make-point-2d :x (/ shigi-screen-width 2)
+                    :y (* shigi-screen-height 1/5))
+     (make-model-2d :model (make-solid-circle :r r :color #x0000ff)
+                    :depth 1
+                    :offset (make-point-2d :x (* -1 r)
+                                           :y (* -1 r))))
+    (add-ecs-entity player)))
+
+;; --- target --- ;;
+
+(defun.ps+ init-dummy-target ()
+  (let ((target (make-ecs-entity))
+        (half-len 25)
+        (angle (* PI 1/3)))
+    (add-entity-tag target :target)
+    (flet ((make-line-model (x1 y1 x2 y2)
+             (make-model-2d
+              :model (make-line :pos-a (list x1 y1)
+                                :pos-b (list x2 y2)
+                                :color #x888888)
+              :depth 0)))
+      (add-ecs-component-list
+       target
+       (make-point-2d :x (* shigi-screen-width 600/1000)
+                      :y (* shigi-screen-height 280/1000))
+       (make-line-model (* -1 half-len) 0 half-len 0)
+       (make-line-model 0 (* -1 half-len) 0 half-len)
+       (make-model-2d :model (make-line :pos-a (list -1000 0)
+                                        :pos-b (list 1000 0)
+                                        :color #xaaaaaa)
+                      :depth -1
+                      :offset (make-point-2d :angle angle)
+                      :label :expected-line))
+      (init-entity-params :angle angle))
+    (add-ecs-entity target)))
+
+;; XXX: These assume that local point is same to global point.
+(defun.ps+ get-target-point (&optional (target (find-target)))
+  (get-ecs-component 'point-2d target))
+
+(defun.ps+ set-target-point (x y &optional (target (find-target)))
+  (with-ecs-components (point-2d) target
+    (setf (point-2d-x point-2d) x
+          (point-2d-y point-2d) y)))
+
+(defun.ps+ get-target-angle (&optional (target (find-target)))
+  (let ((model (find-model-2d-by-label target :expected-line)))
+    (assert model)
+    (point-2d-angle (model-2d-offset model))))
+
+(defun.ps+ set-target-angle (angle &optional (target (find-target)))
+  (let ((model (find-model-2d-by-label target :expected-line)))
+    (assert model)
+    (setf (point-2d-angle (model-2d-offset model))
+          angle)))
+
+(defun.ps+ find-target ()
+  (let ((target (find-a-entity-by-tag :target)))
+    (assert target)
+    target))
--- static/js/test/orbiter.lisp
+++ static/js/test/orbiter.lisp
@@ -6,47 +6,69 @@
         :cl-web-2d-game
         :cl-shigi-simulator/static/js/tools
         :cl-ps-ecs)
-  (:import-from :ps-experiment/common-macros
-                :with-slots-pair))
+  (:import-from :cl-shigi-simulator/static/js/test/orbiter-utils
+                :init-dummy-player
+                :init-dummy-target
+                :get-target-point
+                :set-target-point
+                :get-target-angle
+                :set-target-angle))
 (in-package :cl-shigi-simulator/static/js/test/orbiter)
 
-(defun.ps make-mouse-pointer ()
-  (let* ((pointer (make-ecs-entity))
-         (num-pnts 20)
-         (init-pnt-list '())
-         (r #y10))
-    (dotimes (i num-pnts)
-      (push (list (- 100 (* 10 i)) 0) init-pnt-list))
+(defun.ps+ init-mouse-pointer ()
+  (let ((pointer (make-ecs-entity))
+        (r #y8))
     (add-ecs-component-list
      pointer
-     (make-point-2d :center (make-vector-2d :x r :y r))
-     (make-model-2d :model (make-lines :pnt-list init-pnt-list :color 0xff0000)
+     (make-point-2d)
+     (make-model-2d :model (make-solid-circle :r r :color #xff0000)
                     :depth 1)
      (make-script-2d :func (lambda (entity)
-                             (with-ecs-components (model-2d) entity
-                               (let* ((geometry model-2d.model.geometry)
-                                      (pnt-list geometry.vertices)
-                                      (len (length pnt-list)))
-                                 (with-slots (x y) (aref pnt-list 0)
-                                   (setf x (get-mouse-x)
-                                         y (get-mouse-y)))
-                                 (dotimes (i (1- len))
-                                   (let ((index (- len (1+ i))))
-                                     (with-slots-pair (((x1 x) (y1 y)) (aref pnt-list index)
-                                                       ((x0 x) (y0 y)) (aref pnt-list (1- index)))
-                                       (setf x1 x0 y1 y0))))
-                                 (setf geometry.vertices-need-update t))))))
+                             (with-ecs-components (point-2d) entity
+                               (setf (point-2d-x point-2d) (get-mouse-x)
+                                     (point-2d-y point-2d) (get-mouse-y))))))
     (add-ecs-entity pointer)))
 
-(defun.ps init (scene)
-  (scene.add (make-line :pos-a (list #y1333 #y500) :pos-b (list 0 #y500) :color 0x00ff00))
-  (scene.add (make-line :pos-a (list #y666 #y0) :pos-b (list #y666 #y1000) :color 0x00ff00))
-  (make-mouse-pointer))
+(defun.ps+ init-controller ()
+  (let ((ctr (make-ecs-entity)))
+    (add-ecs-component-list
+     ctr
+     (make-script-2d
+      :func (lambda (entity)
+              (declare (ignore entity))
+              (when (eq (get-left-mouse-state) :down)
+                (set-target-point (get-mouse-x)
+                                  (get-mouse-y)))
+              (let ((diff-angle (* PI 1/60)))
+                (when (> (get-mouse-wheel-delta-y) 0)
+                  (set-target-angle (- (get-target-angle) diff-angle)))
+                (when (< (get-mouse-wheel-delta-y) 0)
+                  (set-target-angle (+ (get-target-angle) diff-angle)))))))
+    (add-ecs-entity ctr)))
 
-(defun.ps main ()
+(defun.ps+ init-background ()
+  (let ((bg (make-ecs-entity)))
+    (add-ecs-component-list
+     bg
+     (make-point-2d)
+     (make-model-2d :model (make-solid-rect :width shigi-screen-width
+                                            :height shigi-screen-height
+                                            :color #xeeeeee)
+                    :depth -1000))
+    (add-ecs-entity bg)))
+
+(defun.ps+ init (scene)
+  (declare (ignore scene))
+  (init-mouse-pointer)
+  (init-background)
+  (init-dummy-player)
+  (init-dummy-target)
+  (init-controller))
+
+(defun.ps+ main ()
   (start-game :screen-width shigi-screen-width
               :screen-height shigi-screen-height
-              :init-function init))
+              :init-function #'init))
 
 (defun js-main ()
   (with-use-ps-pack (:cl-shigi-simulator/static/js/tools
